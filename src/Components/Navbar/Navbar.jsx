919dead577627b9622b5901fb615ee28ea852516
ZTeasley <zwteasley@gmail.com>
2022-12-06 12:51:50 -0700

    React Portfolio With Resume Downloader

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -5,7 +5,7 @@ const Navbar = () => {
   return (
     <div className="n-wrapper">
         <div className="n-left">
-            <div className="n-name">Teasley</div>
+            <div className="n-name">Zion Teasley</div>
             {/* <span>toggle</span> */}
         </div>
         <div className="n-right">
---
+++ src/Components/Services/Services.jsx
@@ -0,0 +1,66 @@
+import React from "react";
+import "./Services.css";
+import HeartEmoji from "../../img/heartemoji.png";
+import Glasses from "../../img/glasses.png";
+import Humble from "../../img/humble.png";
+import Card from "../Card/Card";
+import Resume from "../Services/resume.odt";
+
+const Services = () => {
+  return (
+    <div className="services">
+      {/* left side */}
+      <div className="awesome">
+        <span>My</span>
+        <span>Specialties</span>
+        <span>
+          Lorem ipsum dolor sit, amet consectetur
+          <br />
+          adipisicing elit. Mollitia non quia enim iure saepe.
+        </span>
+
+        <a href={Resume} download>
+          <button className="button s-btton">Download Resume</button>
+        </a>
+
+        <div className="blur s-blur1" style={{ background: "#ABF1FF94" }}></div>
+
+      </div>
+      {/* right side */}
+      <div className="cards">
+        <div style={{ left: "22rem" }}>
+          <Card
+            emoji={HeartEmoji}
+            heading={"Design"}
+            detail={"Figma, Adobe XD, Photoshop"}
+          />
+        </div>
+
+        {/* second card */}
+
+        <div style={{ top: "0rem", left: "8rem" }}>
+          <Card
+            emoji={Glasses}
+            heading={"Front-End"}
+            detail={"HTML, CSS, SASS, JavaScript, ReactJS"}
+          />
+        </div>
+
+        {/* third card */}
+
+        <div style={{ top: "0rem", left: "36rem" }}>
+          <Card
+            emoji={Humble}
+            heading={"UX & UI"}
+            detail={"Material UI, Chakra UI, Bootstrap,"}
+          />
+        </div>
+
+        <div className="blur s-blur2" style={{background: "var(--purple)"}}></div>
+
+      </div>
+    </div>
+  );
+};
+
+export default Services;
