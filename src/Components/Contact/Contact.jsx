4dcfda828e72aa77d9af9b5cb2c52a03fd91e073
ZTeasley <zwteasley@gmail.com>
2022-12-07 09:33:02 -0700

    Added The Contact Me Componet (Works)

--- src/Components/Contact/Contact.jsx
+++ src/Components/Contact/Contact.jsx
@@ -1,10 +1,72 @@
-import React from 'react';
-import './Contact.css';
+import React, { useState } from "react";
+import "./Contact.css";
+import emailjs from '@emailjs/browser';
+import { useRef } from "react";
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
-}
+  const form = useRef();
 
-export default Contact
+  const [done, setDone] = useState(false);
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    emailjs
+      .sendForm(
+        "service_wp9wieh",
+        "template_6lufbsj",
+        form.current,
+        "Q_l8UBH52EE9pd3MF"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+          setDone(true);
+        },
+        (error) => {
+          console.log(error.text);
+        }
+      );
+  };
+
+  return (
+    <div className="contact-form">
+      <div className="w-left">
+        <div className="awesome">
+          <span>Get In Touch</span>
+          <span>Contact Me</span>
+          <div
+            className="blur s-blur"
+            style={{ background: "#ABF1FF94" }}
+          ></div>
+        </div>
+      </div>
+
+      <div className="c-right">
+        <form ref={form} onSubmit={sendEmail}>
+          <input
+            type="text"
+            name="user_name"
+            className="user"
+            placeholder="Name"
+          />
+          <input
+            type="email"
+            name="user_email"
+            className="user"
+            placeholder="Email"
+          />
+          <textarea name="message" className="user" placeholder="Message" />
+          <input type="submit" value="Send" className="button" />
+          <span>{done && "Your Message Has Been Sent"}</span>
+          <div
+            className="blur c-blur1"
+            style={{ background: "var(--purple)" }}
+          ></div>
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Contact;
