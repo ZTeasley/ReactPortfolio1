79a84c21f596a23c878293e9f5f0695105027aff
ZTeasley <zwteasley@gmail.com>
2022-12-09 05:46:18 -0700

    Made Changes To The UI

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,41 +1,54 @@
-import React from 'react';
-import '../Navbar/Navbar.css';
-import {Link} from 'react-scroll';
+import React from "react";
+import "../Navbar/Navbar.css";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
   return (
     <div className="n-wrapper">
-        <div className="n-left">
-            <div className="n-name">Zion Teasley</div>
-            {/* <span>toggle</span> */}
+      <div className="n-left">
+        <div className="n-name">Zion Teasley</div>
+        {/* <span>toggle</span> */}
+      </div>
+      <div className="n-right">
+        <div className="n-list">
+          <ul style={{ listStyleType: "none" }}>
+            <Link
+              spy={true}
+              to="Navbar"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Home</li>
+            </Link>
+
+            <Link
+              spy={true}
+              to="Services"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Specialties</li>
+            </Link>
+
+            {/* <li>Experience</li> */}
+            <Link
+              spy={true}
+              to="Portfolio"
+              smooth={true}
+              activeClass="activeClass"
+            >
+              <li>Projects</li>
+            </Link>
+            {/* <li>Testimonials</li> */}
+          </ul>
         </div>
-        <div className="n-right">
-            <div className="n-list">
-                <ul style={{listStyleType: 'none'}}>
 
-                    <Link spy={true} to='Navbar' smooth={true} activeClass='activeClass'>
-                    <li>Home</li>
-                    </Link>
-
-                    <Link spy={true} to='Services' smooth={true} activeClass='activeClass'>
-                    <li>Specialties</li>
-                    </Link>
-
-                    {/* <li>Experience</li> */}
-                    <Link spy={true} to='Portfolio' smooth={true} activeClass='activeClass'>
-                    <li>Projects</li>
-                    </Link>
-                    {/* <li>Testimonials</li> */}
-                </ul>
-            </div>
-
-            
-            <button className="button n-button">
-                Contact
-            </button>
-        </div>
+        <a href="#contactDiv">
+          <button className="button n-button">Contact</button>
+        </a>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
